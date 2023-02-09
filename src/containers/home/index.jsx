931fe892893b61ef1bfa555199a1c60fb15f01f1
Felix Kwan <fekwan@deloitte.com.hk>
2023-02-09 10:43:46 +0800

    font setup

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,4 +1,3 @@
-import logo from '../../logo.svg';
 import { useTranslation } from 'react-i18next';
 import './style.scss';
 
@@ -6,21 +5,8 @@ const Home = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homepage-container">
+      Homepage Test
     </div>
   );
 };
